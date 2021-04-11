35902d51d191bf74497a598e3a3c41d17dd7daac
noutashm <mojeeb.noutash@gmail.com>
2021-04-11 20:52:25 +1200

    a

--- scripts/assignment.js
+++ scripts/assignment.js
@@ -1,20 +1,22 @@
-var allPages = document.querySelectorAll('#tab-content div[id^="page-"]');
-var nextBtn = document.getElementById('next');
-var previusBtn = document.getElementById('previous');
+var allPages = document.querySelectorAll('#tab-content div[id^="page-"]')
+var nextBtn = document.getElementById('next')
+var previousBtn = document.getElementById('previous')
+var submitBtn = document.getElementById('submit')
+var progressBar = document.getElementById('progress-bar')
 
-var totalPages = allPages.length;
-var currentPageIndex = 0;
+var totalPages = allPages.length
+var currentPageIndex = 0
 
 function hideAllPages(pages) {
     pages.forEach((page) => {
-        page.classList.add('hidden');
-    });
+        page.classList.add('hidden')
+    })
 }
 
 function showAllPages(pages) {
     pages.forEach((page) => {
-        page.classList.remove('hidden');
-    });
+        page.classList.remove('hidden')
+    })
 }
 
 function hideElement(id) {
@@ -22,48 +24,76 @@ function hideElement(id) {
 }
 
 function showElement(id) {
-    document.getElementById(id).classList.remove('hidden');
+    document.getElementById(id).classList.remove('hidden')
 }
 
-hideAllPages(allPages);
-showElement('page-' + (currentPageIndex + 1));
+hideAllPages(allPages)
+showElement('page-' + (currentPageIndex + 1))
 
 if (currentPageIndex == 0) {
-    hideElement('previous');
+    hideElement('previous')
+}
+
+if (currentPageIndex != 4) {
+    hideElement('submit')
 }
 
 nextBtn.onclick = function () {
-    currentPageIndex += 1;
-    hideAllPages(allPages);
-    showElement('page-' + (currentPageIndex + 1));
+    currentPageIndex += 1
+    hideAllPages(allPages)
+    showElement('page-' + (currentPageIndex + 1))
 
     if (currentPageIndex > 0) {
-        showElement('previous');
+        showElement('previous')
     }
 
-    if (currentPageIndex == totalPages - 1) {
-        hideElement('next');
-    }
+    conditions()
 }
 
-previusBtn.onclick = function () {
-    currentPageIndex -= 1;
-    hideAllPages(allPages);
-    showElement('page-' + (currentPageIndex + 1));
+previousBtn.onclick = function () {
+    currentPageIndex -= 1
+    hideAllPages(allPages)
+    showElement('page-' + (currentPageIndex + 1))
 
     if (currentPageIndex == 0) {
-        hideElement('previous');
+        hideElement('previous')
     }
 
     if (currentPageIndex > 0) {
-        showElement('previous');
-    }
-
-    if (currentPageIndex == totalPages - 1) {
-        hideElement('next');
+        showElement('previous')
     }
 
     if (currentPageIndex < totalPages - 1) {
-        showElement('next');
+        showElement('next')
+    }
+
+    conditions()
+}
+
+function conditions() {
+    if (currentPageIndex == totalPages - 1) {
+        hideElement('next')
+        showElement('submit')
+    } else {
+        hideElement('submit')
+    }
+
+    switch (currentPageIndex) {
+        case 0:
+            progressBar.style.width = '25%'
+            progressBar.innerHTML = '25%'
+            break;
+        case 1:
+            progressBar.style.width = '50%'
+            progressBar.innerHTML = '50%'
+            break;
+        case 2:
+            progressBar.style.width = '75%'
+            progressBar.innerHTML = '75%'
+            break;
+        case 3:
+            progressBar.style.width = '100%'
+            progressBar.innerHTML = '100%'
+            break;
     }
 }
